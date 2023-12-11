872c8c9caa5a57960e1742ea5a373d0e02e5eae0
M. Tirado <mtirado1999@hotmail.com>
2023-12-10 20:10:07 -0800

    Fix install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # Lua modules installation path, should be in your $LUA_PATH
-LUA_DIR="/usr/local/share/lua/5.3"
+LUA_DIR="$PWD"
 # Script installation path
-INSTALL_DIR="/usr/local/bin"
+INSTALL_DIR=~/.local/bin
 
 cp -r pantograph "$LUA_DIR"
 echo "#!/bin/sh" > pantograph.sh
-echo "lua $LUA_DIR/pantograph/main.lua \$@" > pantograph.sh
+echo "lua $LUA_DIR/pantograph/main.lua \$@" >> pantograph.sh
 chmod +x pantograph.sh
 mv pantograph.sh "$INSTALL_DIR/pantograph"
